17f361ebafcea3fba10f5a2d81e9789b22176d57
Ampofo Twene <ampofodrum1@gmail.comAmpofo-langgit config --global user.name Ampofo-langqgit config --global user.email ampofodrum1@gmail.com>
2019-11-27 19:02:51 +0000

    Edit line 124, 130

--- bikeshare.py
+++ bikeshare.py
@@ -121,13 +121,13 @@ def station_stats(df):
     print('\nCalculating The Most Popular Stations and Trip...\n')
     start_time = time.time()
 
-    # display most commonly used start station
+    # display the commonest used start station
 
     commonnest_start_station = df['Start Station'].value_counts().idxmax()
     print("The most commonly used start station :", commonnest_start_station)
 
 
-    # display most commonly used end station
+    # display the  commonnest used end station
 
     commonnest_end_station = df['End Station'].value_counts().idxmax()
     print("The most commonly used end station :", commonnest_end_station)
